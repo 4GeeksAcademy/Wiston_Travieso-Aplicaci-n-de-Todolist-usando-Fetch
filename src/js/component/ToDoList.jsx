8ec7255af6835027be42a6308c06f926756f8239
WistonTravieso <wistontravieso@gmail.com>
2024-11-20 00:13:56 +0000

    [Entrega final]

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,89 +1,124 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 const ToDoList = () => {
-    const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState({
+        label: "",
+        is_done: false
+    });
     const [todos, setTodos] = useState([])
 
 
-    function getTodos() {
-        fetch("https://playground.4geeks.com/todo/users/wiston_travieso")
-            .then((response) => {
-                console.log(response.ok);
-                console.log(response.status);
-                console.log(response.text());
-                return response.json()
-            })
-            .then(data => {
-                setTodos(data.todos);
-                console.log(data);
-            })
-            .catch((err) => { console.log(err) })
+    async function getTodos() {
+        try {
+
+            const response = await fetch("https://playground.4geeks.com/todo/users/wiston_travieso")
+            const data = await response.json()
+
+            if (response.ok) {
+                setTodos(data.todos)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+        // fetch("https://playground.4geeks.com/todo/users/wiston_travieso")
+        //     .then((response) => {
+        //         console.log(response.ok);
+        //         console.log(response.status);
+        //         console.log(response.text());
+        //         return response.json()
+        //     })
+        //     .then(data => {
+        //         setTodos(data.todos);
+        //         console.log(data);
+        //         console.log("data desde get");
+        //     })
+        //     .catch((err) => { console.log(err) })
     }
 
-    function addTask(inputValue, e) {
-        setTodos(todos.concat(inputValue))
-        e.target.value = ""
-    }
-    function addTodo(inputValue) {
-        if (inputValue !== "") {
-            let newTask = {
-                label: inputValue,
-                is_done: false
-            }
-            fetch("https://playground.4geeks.com/todo/todos/wiston_travieso", {
-                method: "POST",
-                body: JSON.stringify(newTask),
-                headers: { "Content-Type": "application/json" }
+
+    async function deleteTask(id) {
+        try {
+            const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`,{
+                method:"DELETE"
             })
-                .then((response) => {
-                    response.json()
-                })
-                .then((data) => {
-                    setTodos((prevTodos) => [...prevTodos, newTask]);
-                    console.log()
-                })
-                .catch((err) => { console.log(err) })
+          if (response.ok){
+            getTodos()
+          }            
+        } catch (error) {
+            console.log(error)
         }
     }
 
-
-
-
-    function deleteTask(task, index) {
-        setTodos(todos.filter((task, taskIndex) => index != taskIndex))
+    function handleChange(e) {
+        setInputValue({
+            ...inputValue,
+            [e.target.name]: e.target.value
+        })
     }
+    async function saveTask(e) {
+        if (e.key == "Enter") {
+            try {
+                let response = await fetch("https://playground.4geeks.com/todo/todos/wiston_travieso",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(inputValue)
+                    }
+                )
+                if (response.ok) {
+                    getTodos()
+                }
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+    }
+
+
+    useEffect(() => {
+        console.log("me ejecuto")
+        getTodos()
+    }, [])
 
     return (
         <div className="container col-6">
             <div className="container">
 
                 <input className="form-control" type="text"
-                    onChange={(e) => {
-                        setInputValue(e.target.value);
-                    }}
-                    //value={inputValue}
-                    onKeyUp={(e) => {
-                        e.key === "Enter"
-                            ? addTask(inputValue, e)
-                            : null
-                    }
-                    } placeholder="Que necesitas Hacer?">
+                    onChange={handleChange}
+                    value={inputValue.label}
+                    name="label"
+
+                    onKeyDown={saveTask}
+
+                    placeholder="Que necesitas Hacer?">
 
                 </input>
 
                 <ul className="list-group">
-                    {todos.map((task, index) => (
-                        <li className="list-group-item" key={index}
-                        >{task}  <button type="button" class="btn btn-dark"
-                            onClick={(e) =>
-                                deleteTask(task, index)}
-                        >X</button>
+                    {todos.map((task) => (
+                        <li
+                            className="list-group-item"
+                            key={task.id}
+                        >
+                            {task.label}
+                            <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => deleteTask(task.id)}
+                            >X</button>
                         </li>
                     ))}
                     <div className="text-align-right">
                         {todos.length} item left
                     </div>
+
                 </ul>
             </div>
         </div >
